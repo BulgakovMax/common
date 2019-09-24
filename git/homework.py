8b45c3794d69b9c62019d1a43acb6158ba4d620c
Max Bulgakov <max.bulgakov.v@gmail.com>
2019-09-24 21:48:05 +0300

    "Fix remarks"

--- git/homework.py
+++ git/homework.py
@@ -16,6 +16,7 @@ def is_two_object_has_same_value(first: Any, second: Any) -> bool:
     If @first and @second has same value should return True
     In another case should return False
     """
+    pass
     return first == second
 
 
@@ -24,6 +25,7 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
+    pass
     return type(first) == type(second)
 
 
@@ -32,6 +34,7 @@ def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     If @first and @second has same type should return True
     In another case should return False
     """
+    pass
     return  first is second
 
 
@@ -49,6 +52,7 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
+    pass
     if isinstance(first_value, int) and isinstance(second_value, int):
         return first_value * second_value
     else:
@@ -82,6 +86,7 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
+    pass
     try:
         return int(first_value) * int(second_value)
     except ValueError:
@@ -103,6 +108,7 @@ def is_word_in_text(word: str, text: str) -> bool:
         is_word_in_text("Glad", "Nice to meet you ")
         >>> False
     """
+    pass
     return word in text
 
 
@@ -110,6 +116,7 @@ def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
+    pass
     return [i for i in range(13) if i not in [6, 7]]
 
 
@@ -122,6 +129,7 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
+    pass
     return [i for i in data if i >= 0]
 
 
@@ -133,6 +141,7 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
+    pass
     d = dict.fromkeys(string.ascii_lowercase, 1)
     i = 0
     for k, v in d.items():
@@ -149,6 +158,7 @@ def simple_sort(data: List[int]) -> List[list]:
         >>> [1, 2, 2, 3, 6, 7, 9]
     Returns:
     """
+    pass
     n = len(data)
     for i in range(n):
         for j in range(0, n - i - 1):
