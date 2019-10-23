6279abde1e96b4a83c41acd523956bac00f5e3ea
BulgakovMax <max.bulgakov.v@gmail.com>
2019-10-24 00:23:30 +0300

    HW done!

---
+++ Flask/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main_page():
+    return render_template('home.html')
+
+
+@app.route('/base')
+def base_page():
+    return render_template('base.html')
+
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/vegetables')
+def vegetables_page():
+    list = ['beans', 'carrot', 'beetroot', 'cucumber']
+    return render_template('vegetables.html', list=list)
+
+
+@app.route('/fruits')
+def fruits_page():
+    list = ['melon', 'apple', 'strawberry', 'grape']
+    return render_template('fruits.html', list=list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
