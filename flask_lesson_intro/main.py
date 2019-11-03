e9ca9723f85895ca81f86aa22ae32e042be8c4b4
Max Bulgakov <max.bulgakov.v@gmail.com>
2019-11-03 20:41:22 +0200

    Add dynamic page generator from data in json file

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -14,34 +14,15 @@ def get_author_page():
     return render_template("author.html", data=get_data())
 
 
-@app.route('/Alarm_clock')
-def get_alarm_clock_page():
-    return render_template("Alarm_clock.html", data=get_data())
-
-
-@app.route('/Headphones')
-def get_headphones_page():
-    return render_template("Headphones.html", data=get_data())
-
-
-@app.route('/IPod')
-def get_ipod_page():
-    return render_template("IPod.html", data=get_data())
-
-
-@app.route('/Calculator')
-def get_calculator_page():
-    return render_template("Calculator.html", data=get_data())
-
-
-@app.route('/Coffeemaker')
-def get_coffemaker_page():
-    return render_template("Coffeemaker.html", data=get_data())
-
-
-@app.route('/Battery_charger')
-def get_battery_charger_page():
-    return render_template("Battery_charger.html", data=get_data())
+@app.route('/<item>')
+def get_item_page(item):
+    for i in get_data():
+        if i['title'] == item:
+            return render_template("item.html",
+                                   title=i['title'],
+                                   text=i['text'],
+                                   image=i['image'],
+                                   data=get_data())
 
 
 if __name__ == "__main__":
